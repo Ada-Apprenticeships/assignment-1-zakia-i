0dea6868224a98e82416a3e18ab22fc8815b3041
zakia-i <zakia.irmal@ada.ac.uk>
2025-02-02 17:06:58 +0000

    Primer 3: Formatting

--- primer3/LinkedList.js
+++ primer3/LinkedList.js
@@ -1,43 +1,44 @@
 class Node {
-    constructor(data) {
-      this.data = data;
-      this.next = null;
-    };
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   };
-  
-  class LinkedList {
-    constructor() {
-      this.head = null;
-    };
-  
-    append(post) {
-      const newNode = new Node(post);
-      if (!this.head) {
-        this.head = newNode;
-      } else {
-        let current = this.head;
-        while (current.next) {
-          current = current.next;
-        };
-        current.next = newNode;
-      };
-    };
-  
-    search(phrase) {
-      let results = [];
+};
+
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  };
+
+  append(post) {
+    const newNode = new Node(post);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
       let current = this.head;
-      const keywords = phrase.toLowerCase().split(' ');
-  
-      while (current) {
-        const lowerText = current.data.text.toLowerCase();
-  
-        if (keywords.some(keyword => lowerText.includes(keyword))) {
-          results.push(current.data);
-        };
+      while (current.next) {
         current = current.next;
       };
-      return results;
+      current.next = newNode;
     };
   };
-  
-  export { LinkedList };
+
+  search(phrase) {
+    let results = [];
+    let current = this.head;
+    const keywords = phrase.toLowerCase().split(' ');
+
+    while (current) {
+      const lowerText = current.data.text.toLowerCase();
+
+      if (keywords.some(keyword => lowerText.includes(keyword))) {
+        results.push(current.data);
+      };
+      current = current.next;
+    };
+    return results;
+  };
+};
+
+export { LinkedList };
